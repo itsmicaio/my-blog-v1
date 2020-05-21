ab0d38b0d176e0a5900886a7e8ac22e1aa9517a3
Caio Fuzatto Mendonça <caio.mendonca@flexait.com.br>
2020-05-21 17:14:36 -0300

    Creating profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Caio Fuzatto</h1>
+        <h2>Co-Founder and Software Enginner at VilaApps</h2>
+        <p>
+            Responsável pelas aplicações da VilaApps, entusiasta de tecnologias e um cara legal 
+        </p>
+    </div>
+)
+
+export default Profile
