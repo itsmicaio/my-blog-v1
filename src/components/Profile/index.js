b0dab665ee6fd6b53bb723a8e541abcb70f8e886
Caio Fuzatto Mendonça <caio.mendonca@flexait.com.br>
2020-05-22 10:44:28 -0300

    Creating profile component style

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useStaticQuery, graphql} from 'gatsby'
 import Avatar from '../Avatar'
 
+import * as S from './styled'
+
 const Profile = () => {
   const {site: 
     {siteMetadata: {title, position, description}}
@@ -18,14 +20,18 @@ const Profile = () => {
   `)
     
   return (
-    <div className="Profile-wrapper">
-      <Avatar />
-      <h1>{title}</h1>
-      <h2>{position}</h2>
-      <p>
+    <S.ProfileWrapper>
+      <S.ProfileLink>
+        <Avatar />
+        <S.ProfileAuthor>
+          {title}
+          <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+      </S.ProfileLink>
+      <S.ProfileDescription>
         {description} 
-      </p>
-    </div>
+      </S.ProfileDescription>
+    </S.ProfileWrapper>
   )
 }
 
