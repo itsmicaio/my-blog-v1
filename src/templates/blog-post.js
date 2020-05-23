d88d7f6db939540496293c420eba5eb131a926a7
Caio Fuzatto Mendonça <caio.mendonca@flexait.com.br>
2020-05-23 19:34:47 -0300

    Adding style at blog-post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+import * as S from "../components/Post/styled"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+    </Layout>
   )
 }
 
@@ -16,9 +30,12 @@ export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        description
         title
       }
       html
+      timeToRead
     }
   }
 `
