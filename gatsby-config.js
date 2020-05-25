27d955df80be774e424842cd37ff293f7f478e9b
Caio Fuzatto Mendonça <caio.mendonca@flexait.com.br>
2020-05-25 12:24:49 -0300

    Configure upload posts to algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+const queries = require('./src/utils/algolia_queries')
 module.exports = {
   siteMetadata: {
     title: `Um Jovem Empreendesenvolvedor`,
@@ -53,6 +56,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
