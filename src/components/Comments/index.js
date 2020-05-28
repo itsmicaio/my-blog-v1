3c42c7637a2330b4e975558d9e19b78b4301faa7
Caio Fuzatto <caio.mendonca@vilapps.com.br>
2020-05-28 11:06:10 -0300

    Configure disqus

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,13 +5,13 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://itsmicaio.com.br${url}`
+  const completeURL = `https://itsmicaio.netlify.app${url}`
 
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="willianjusten"
+        shortname="itsmicaio"
         identifier={completeURL}
         title={title}
         url={completeURL}
