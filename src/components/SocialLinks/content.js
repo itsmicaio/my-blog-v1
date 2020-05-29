2a2d3622a3f689192900e02cc490a6f36a380678
Caio Fuzatto <caio.mendonca@vilapps.com.br>
2020-05-28 21:02:38 -0300

    Adding linkedin at social links

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -9,18 +9,22 @@ const links = [
     label: "Twitter",
     url: `https://twitter.com/${handle}`,
   },
-  {
-    label: "Youtube",
-    url: `https://www.youtube.com/${handle}`,
-  },
+  // {
+  //   label: "Youtube",
+  //   url: `https://www.youtube.com/${handle}`,
+  // },
   {
     label: "Instagram",
     url: `https://www.instagram.com/${handle}`,
   },
   {
-    label: "Unsplash",
-    url: `https://unsplash.com/${handle}`,
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/caio-fuzatto-946bbb165/`,
   },
+  // {
+  //   label: "Unsplash",
+  //   url: `https://unsplash.com/${handle}`,
+  // },
 ]
 
 export default links
